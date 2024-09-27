f3ca2c18a9498bd248e4f563ebe93905846589c0
eramp <ogfnzerogravity@gmail.com>
2024-09-27 11:34:15 -0300

    editing! working!

--- 10.40-GameserverV2/Player.h
+++ 10.40-GameserverV2/Player.h
@@ -27,6 +27,39 @@ void ServerExecuteInventoryItem(AFortPlayerControllerAthena* PC, FGuid Guid)
     }
 }
 
+static void ServerBeginEditingBuildingActorHook(AFortPlayerController* PlayerController, ABuildingSMActor* BuildingActorToEdit)
+{
+	auto Pawn = PlayerController->MyFortPawn;
+
+	if (!Pawn)
+		return;
+
+	auto PlayerState = Cast<AFortPlayerStateZone>(Pawn->PlayerState);
+
+	if (!PlayerState)
+		return;
+
+	/* if (Pawn->CurrentWeapon)
+	{
+		if (Pawn->CurrentWeapon->IsA(AFortWeap_BuildingToolBase::StaticClass()))
+			return;
+	} */
+
+	if (BuildingActorToEdit && BuildingActorToEdit->bPlayerPlaced)
+	{
+		static auto EditToolDef = UObject::FindObject<UFortWeaponItemDefinition>("/Game/Items/Weapons/BuildingTools/EditTool.EditTool");
+
+		if (auto EditTool = Cast<AFortWeap_EditingTool>(Pawn->EquipWeaponDefinition(EditToolDef, FGuid{})))
+		{
+			BuildingActorToEdit->EditingPlayer = Cast<AFortPlayerStateZone>(Pawn->PlayerState);
+			BuildingActorToEdit->OnRep_EditingPlayer();
+
+			EditTool->EditActor = BuildingActorToEdit;
+			EditTool->OnRep_EditActor();
+		}
+	}
+}
+
 namespace Player
 {
     void HookPlayer()
@@ -36,5 +69,6 @@ namespace Player
 
         VirtualHook(FortPlayerControllerAthenaDefault->VFT, 264, ServerAcknowlegePossession);
         VirtualHook(FortPlayerControllerAthenaDefault->VFT, 510, ServerExecuteInventoryItem);
+        VirtualHook(FortPlayerControllerAthenaDefault->VFT, 555, ServerBeginEditingBuildingActorHook, nullptr);
     }
 }
